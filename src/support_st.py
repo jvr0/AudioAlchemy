5aee3dbc865eb2304f2034ced639d3e829ab525e
Joaquin Villaverde Roldan <joaquin@MacBook-Air-de-Joaquin.local>
2023-12-03 18:31:38 +0100

    ruta

--- src/support_st.py
+++ src/support_st.py
@@ -3,12 +3,12 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('../data/inventario.csv')
+df = pd.read_csv('data/inventario.csv')
 df = df[df.status == 'For Sale']
 
 # La siguiente función permite que cuando se inicia el st tener un dato actualizado del inventario que estamos manejando
 def tamaño_inventario ():
-    df = pd.read_csv('../data/inventario.csv')
+    df = pd.read_csv('data/inventario.csv')
     df = df[df.status == 'For Sale']
     return(len(df))
 
