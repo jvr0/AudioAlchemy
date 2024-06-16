f29f8142ac9ffe330e0945d7d9773391281951e5
Joaquin Villaverde Roldan <joaquin@MacBook-Air-de-Joaquin.local>
2024-06-16 19:46:57 +0200

    hardcodeo de funcionalidad

--- main.py
+++ main.py
@@ -31,46 +31,40 @@ if user_input == password:
     password_placeholder.empty()
     size = tamaño_inventario_venta()
     st.set_option('deprecation.showPyplotGlobalUse', False)
-    
-    
 
     if 'programacion_activa' not in st.session_state:
         st.session_state['programacion_activa'] = False    
+
     # MURO 
+    st.write(f'## Programar actualización')
 
-    # PROGRAMAR ACTUALIZACIÓN
+    # Widget para seleccionar la hora
+    hora = st.slider("Selecciona la hora", 0, 23, 9)  # Valores de 0 a 23 para las horas
 
-        # PROGRAMAR ACTUALIZACIÓN
+    def activar_programacion(hora):
+        schedule.every().day.at(f"{hora:02d}:00").do(lanzamiento_programado).tag(f"{hora}")
+        st.session_state['programacion_activa'] = True
 
-        st.write(f'## Programar actualización')
+    def cancelar_programacion():
+        schedule.clear()
+        st.session_state['programacion_activa'] = False
 
-        # Widget para seleccionar la hora
-        hora = st.slider("Selecciona la hora", 0, 23, 9)  # Valores de 0 a 23 para las horas
+    if st.button(":green[Programar]"):
+        activar_programacion(hora)
 
-        def activar_programacion(hora):
-            global programacion_activa
-            schedule.every().day.at(f"{hora:02d}:00").do(lanzamiento_programado).tag(f"{hora}")
+    if st.button(":red[Cancelar]"):
+        cancelar_programacion()
 
-        def cancelar_programacion():
-            global programacion_activa
-            schedule.clear()
+    st.write(schedule.get_jobs())
 
-        if st.button(":green[Programar]"):
-            activar_programacion(hora)
-            st.session_state['programacion_activa'] = True
+    if st.session_state['programacion_activa']:
+        st.write(f':green[Estado de la programación: {st.session_state["programacion_activa"]}]')
+    else:
+        st.write(f':red[Estado de la programación: {st.session_state["programacion_activa"]}]')
 
-        if st.button(":red[cancelar]"):
-            cancelar_programacion()
-            st.session_state['programacion_activa'] = False
-
-        st.write(schedule.get_jobs())
-
-        if st.session_state['programacion_activa']:
-            st.write(f':green[Estado de la programación: {st.session_state["programacion_activa"]}]')
-        else:
-            st.write(f':red[Estado de la programación: {st.session_state["programacion_activa"]}]')
-
-    while st.session_state['programacion_activa'] == True:
+    # Esta sección debería ser manejada en un thread separado o usando async programming
+    # ya que puede hacer que Streamlit se congele.
+    while st.session_state['programacion_activa']:
         schedule.run_pending()
         time.sleep(2)
 
@@ -82,9 +76,7 @@ if user_input == password:
     size = tamaño_inventario_venta()
     st.sidebar.write(f'### Tamaño inventario: {size}')
 
-
     # CONTROL DEL INVENTARIO
-
     if st.sidebar.button('Pedir nuevo inventario a discogs'):
         new = nuevo_inventario()
         st.sidebar.info(new)
@@ -103,14 +95,11 @@ if user_input == password:
     st.sidebar.write('---')
 
     # LINKS
-
     st.sidebar.write('---')
-
     if st.sidebar.button(':blue[Personal Links]'):
         st.sidebar.write('https://github.com/jvr0')
         st.sidebar.write('https://www.linkedin.com/in/joaquín-villaverde-roldán-4b9803230')
         st.sidebar.write('https://github.com/jvr0/AudioAlchemy')
-
 
 elif user_input != "" and user_input != password:
     st.error("Contraseña incorrecta. Por favor, intenta nuevamente.")
